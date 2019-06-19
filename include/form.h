16d520b42b03d4e230d8b8ffa39f1b512e2c7092
Talgat <tairovtalgat@gmail.com>
2019-06-20 01:35:07 +0600

    generate in separate thread

--- include/form.h
+++ include/form.h
@@ -3,6 +3,8 @@
 
 #include <QWidget>
 #include <QFile>
+#include <QProgressBar>
+#include <QThread>
 
 #include "name_generator.h"
 #include "structures/hashtable.h"
@@ -18,11 +20,17 @@ class Form : public QWidget
     Q_OBJECT
 
 private:
+    class GenerateThread;
+    friend class GenerateThread;
+
+
     Ui::Form* ui;
     slice::NameGenerator mGenerator;
     slice::HashTable<QString, int> mTable;
+    QFile mFile;
+    QProgressBar mBar;
 
-    QFile file;
+    GenerateThread* mThread;
 
 
 public:
@@ -31,10 +39,31 @@ public:
 
 private slots:
     void on_generateButton_clicked();
-    void on_clearButton_clicked();
-    void on_searchButton_clicked();
     void on_testButton_clicked();
+    void on_searchButton_clicked();
+
+    void on_clearButton_clicked();
     void on_stateButton_clicked();
+
+    void setEnabledButtons();
+};
+
+
+class Form::GenerateThread : public QThread
+{
+   Q_OBJECT
+private:
+    Form* form;
+
+public:
+    GenerateThread() = default;
+    GenerateThread(Form* form);
+
+protected:
+   virtual void run() override;
+
+signals:
+   void ticks(int);
 };
 
 #endif // FORM_H_INCLUDED
--- src/form.cpp
+++ src/form.cpp
@@ -1,4 +1,3 @@
-#include <QProgressBar>
 #include <QIntValidator>
 #include <QDebug>
 
@@ -8,22 +7,35 @@
 
 
 Form::Form(QWidget* parent) : QWidget(parent),  ui(new Ui::Form()),
-    mTable(), file("queries.txt")
+    mTable(251), mFile("queries.txt"), mBar(this)
 {
     ui->setupUi(this);
+    resize(width() + 100, height());
     ui->numberGenerateEdit->setValidator(new QIntValidator(0, std::numeric_limits<int>::max(),
                                                            ui->numberGenerateEdit));
     ui->label->setBuddy(ui->additionalTextArea);
     ui->splitter->setStretchFactor(0, 10);
     ui->splitter->setStretchFactor(1, 5);
 
-    resize(width() + 100, height());
-
 
     const int tabStop = 8;
     QFontMetrics metrics(ui->mainTextArea->font());
     ui->mainTextArea->setTabStopWidth(tabStop * metrics.width(' '));
     ui->additionalTextArea->setTabStopWidth(tabStop * metrics.width(' '));
+
+
+    //Progress bar
+    mBar.setRange(0, 100);
+    ui->gridLayout->addWidget(&mBar, 1, 0, 1, 1);
+    mBar.hide();
+
+
+    //Thread
+    mThread = new GenerateThread(this);
+    connect(mThread, &GenerateThread::ticks, &mBar, &QProgressBar::setValue);
+    connect(mThread, &GenerateThread::started, &mBar, &QProgressBar::show);
+    connect(mThread, &GenerateThread::finished, &mBar, &QProgressBar::hide);
+    connect(mThread, &GenerateThread::finished, this, &Form::setEnabledButtons);
 }
 
 
@@ -31,6 +43,7 @@ Form::Form(QWidget* parent) : QWidget(parent),  ui(new Ui::Form()),
 Form::~Form()
 {
     dumpObjectTree();
+    delete mThread;
     delete ui;
 }
 
@@ -38,45 +51,68 @@ Form::~Form()
 
 void Form::on_generateButton_clicked()
 {
-    QVector<QString> homies;
-    for (QString line : ui->additionalTextArea->toPlainText().split("\n"))
+    mThread->start();
+}
+
+
+
+void Form::on_testButton_clicked()
+{
+    ui->mainTextArea->clear();
+    QString text;
+    mFile.open(QIODevice::ReadOnly | QIODevice::Text);
+    QTextStream in(&mFile);
+    QElapsedTimer timer;
+
+    QString line, key;
+    while (!(line = in.readLine()).isNull())
     {
-        if (!line.isEmpty())
+        try
         {
-            QStringList parts = line.split("|");
-            mTable.put(parts.first().trimmed(), parts.last().toInt());
-        };
-    }
+            key = line.split("|").first();
+            timer.restart();
+            int value = mTable.get(key);
+            quint64 nanos = timer.nsecsElapsed();
+            quint64 millis = nanos / 1000000;
 
-
-
-    file.open(QIODevice::WriteOnly | QIODevice::Text);
-    QTextStream out(&file);
-
-    QProgressBar bar(this);
-    bar.setRange(0, 100);
-    bar.setValue(0);
-    ui->gridLayout->addWidget(&bar, 1, 0, 1, 1);
-    bar.show();
-
-    uint count = ui->numberGenerateEdit->text().toInt();
-    uint k = count / 100;
-
-    for (uint i = 0; i < count; i++)
-    {
-        QString person = mGenerator.getRandomPerson().toString();
-        mTable.put(person, mGenerator.rand->bounded(-5000, 25000));
-                if (i % k == 0)
+            text.append("{key:\"" + key + "\", value:" + QString::number(value) + "}, time:"
+                        + QString::number(millis) + "ms " + QString::number(nanos) + "ns\n");
+        }
+        catch (std::out_of_range ex)
         {
-            out << person << "|" << i << endl;
-            bar.setValue(bar.value() + 1);
+            text.append(QString("out_of_range exception, msg:") + ex.what() + "\n");
         }
     }
 
-    file.close();
-    ui->gridLayout->removeWidget(&bar);
-    ui->searchButton->setEnabled(true);
-    ui->testButton->setEnabled(true);
+    ui->mainTextArea->append(text);
+    mFile.close();
+}
+
+
+
+void Form::on_searchButton_clicked()
+{
+    ui->mainTextArea->clear();
+    QString text;
+
+    try
+    {
+        QString key = ui->searchEdit->text();
+        QElapsedTimer timer;
+        timer.start();
+        int value = mTable.get(key);
+        quint64 nanos = timer.nsecsElapsed();
+        quint64 millis = nanos / 1000000;
+
+        text = ("{key:\"" + key + "\", value:" + QString::number(value) + "}, time:"
+                    + QString::number(millis) + "ms " + QString::number(nanos) + "ns\n");
+    }
+    catch (std::out_of_range ex)
+    {
+        text = QString("out_of_range exception, msg:") + ex.what() + "\n";
+    }
+
+    ui->mainTextArea->append(text);
 }
 
 
@@ -92,71 +128,63 @@ void Form::on_clearButton_clicked()
 
 
 
-void Form::on_searchButton_clicked()
-{
-    ui->mainTextArea->clear();
-    QString text;
-    QString key = ui->searchEdit->text();
-
-    try
-    {
-        QElapsedTimer timer;
-        timer.start();
-        int value = mTable.get(key);
-        quint64 nanos = timer.nsecsElapsed();
-        quint64 millis = nanos / 1000000;
-
-        text.append("{key:" + key + ", value:" + QString::number(value) + "}, time:"
-                    + QString::number(millis) + "ms " + QString::number(nanos) + "ns\n");
-    }
-    catch (std::out_of_range ex)
-    {
-        text = QString("out_of_range exception, msg:") + ex.what() + "\n";
-    }
-
-    ui->mainTextArea->append(text);
-}
-
-
-
-void Form::on_testButton_clicked()
-{
-    ui->mainTextArea->clear();
-    QString text;
-    file.open(QIODevice::ReadOnly | QIODevice::Text);
-    QTextStream in(&file);
-    QElapsedTimer timer;
-
-    QString line, key;
-    while (!(line = in.readLine()).isNull())
-    {
-        try
-        {
-            key = line.split("|").first();
-            timer.restart();
-            int value = mTable.get(key);
-            quint64 nanos = timer.nsecsElapsed();
-            quint64 millis = nanos / 1000000;
-
-            text.append("{key:" + key + ", value:" + QString::number(value) + "}, time:"
-                        + QString::number(millis) + "ms " + QString::number(nanos) + "ns\n");
-        }
-        catch (std::out_of_range ex)
-        {
-            text.append(QString("out_of_range exception, msg:") + ex.what() + "\n");
-        }
-    }
-
-    ui->mainTextArea->append(text);
-    file.close();
-}
-
-
-
 void Form::on_stateButton_clicked()
 {
     QString text;
     text.append(QString("{items:") + QString::number(mTable.size()) + ", tableSize:" +
-            QString::number(mTable.sizeTable()) + "}\n");
+                QString::number(mTable.sizeTable()) + "}\n");
     ui->mainTextArea->append(text);
 }
+
+
+
+void Form::setEnabledButtons()
+{
+   ui->searchButton->setEnabled(true);
+   ui->testButton->setEnabled(true);
+}
+
+
+
+
+
+//GenerateThread-----------------------------------
+Form::GenerateThread::GenerateThread(Form* form)
+{
+    this->form = form;
+}
+
+
+
+void Form::GenerateThread::run()
+{
+    for (QString line : form->ui->additionalTextArea->toPlainText().split("\n"))
+    {
+        if (!line.isEmpty())
+        {
+            QStringList parts = line.split("|");
+            form->mTable.put(parts.first().trimmed(), parts.last().toInt());
+        };
+    }
+
+    form->mFile.open(QIODevice::WriteOnly | QIODevice::Text);
+    QTextStream out(&form->mFile);
+
+    uint count = form->ui->numberGenerateEdit->text().toInt();
+    uint k = count / 100;
+
+    int tick = 1;
+
+    for (uint i = 0; i < count; i++)
+    {
+        QString person = form->mGenerator.getRandomPerson().toString();
+        form->mTable.put(person, form->mGenerator.rand->bounded(-5000, 25000));
+        if (i % k == 0)
+        {
+            out << person << "|" << i << endl;
+            emit ticks(tick++);
+        }
+    }
+
+    form->mFile.close();
+ }
